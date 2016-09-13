44ce827f400083e1440652e9598e7ddd8044824f
tang6723 <tang6723@hotmail.com>
2016-09-13 20:53:08 +0800

    Tang

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,7 +1,6 @@
-import {Component} from '@angular/core';
-//import {NavController} from 'ionic-angular';
-import {NavController, Slides} from 'ionic-angular';
-import { ViewChild } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NavController, Slides, NavParams} from 'ionic-angular';
+
 
 
 @Component({
@@ -9,23 +8,14 @@ import { ViewChild } from '@angular/core';
 })
 export class HomePage {
 
+  @ViewChild("myClider") slider: Slides;
 
-  slides = [
-    {
-      title: "Welcome to the Docs!",
-      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
-      image: "img/ica-slidebox-img-1.png",
-    },
-    {
-      title: "What is Ionic?",
-      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
-      image: "img/ica-slidebox-img-2.png",
-    },
-    {
-      title: "What is Ionic Cloud?",
-      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
-      image: "img/ica-slidebox-img-3.png",
-    }
-  ];
+
+  constructor(private navCtrl: NavController , private  navParams: NavParams) {
+  }
+
+  gotoSlider(i: number){
+    this.slider.slideTo(i, 300);
+  }
 
 }
