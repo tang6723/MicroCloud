6fef1b54da7fd4f5f26221904d38e627246e3994
tang6723 <tang6723@hotmail.com>
2016-09-14 10:16:32 +0800

    tang

---
+++ app/pages/store/shop/shop.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ShopPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/shop/shop.html',
+})
+export class ShopPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
