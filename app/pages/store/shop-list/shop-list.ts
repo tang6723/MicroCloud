a01ae1673bfb75d16c4206e16cc828ce547b43b0
tang6723 <tang6723@hotmail.com>
2016-09-14 10:23:21 +0800

    tang

--- app/pages/store/shop-list/shop-list.ts
+++ app/pages/store/shop-list/shop-list.ts
@@ -8,7 +8,7 @@ import { NavController } from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/shop-list/shop-list.html',
+  templateUrl: 'build/pages/store/shop-list/shop-list.html',
 })
 export class ShopListPage {
 
